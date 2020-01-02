423cd2042d638b450f6f5fba03d11e49c11dec79
Steve Schroeder <sschroeder@fr.com>
2020-01-02 11:52:16 -0600

    initial commit

---
+++ scripts/sandbox.js
@@ -0,0 +1,30 @@
+let result =  0 / 0;
+
+console.log(Number.isNaN(result));
+
+// let person = {
+//     name: "John",
+//     age: 32,
+//     partTime: false
+// };
+//
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.partTime);
+
+// function changeCard(card) {
+//     card.suit = "Clubs";
+// }
+//
+// let cards = [
+//     {
+//         suit: "Hearts",
+//         value: "Queen"
+//     },
+//     {
+//         suit: "Clubs",
+//         value: "King"
+//     }
+// ];
+//
+// console.log(cards[0].value);
