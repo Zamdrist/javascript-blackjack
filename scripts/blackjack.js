19968638b69cf4a0c5358bc8968199dcb3c43d58
Steve Schroeder <sschroeder@fr.com>
2020-01-02 16:03:54 -0600

    ignore idea folder

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -3,13 +3,10 @@
 let suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
 let values = ["Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"];
 
-let textArea = document.getElementById("text-area");
-let newGameButton = document.getElementById("new-game-button");
-let hitButton = document.getElementById("hit-button");
-let stayButton = document.getElementById("stay-button");
-
-hitButton.style.display = "none";
-stayButton.style.display = "none";
+let textArea = document.getElementById("text-area"),
+    newGameButton = document.getElementById("new-game-button"),
+    hitButton = document.getElementById("hit-button"),
+    stayButton = document.getElementById("stay-button");
 
 newGameButton.addEventListener("click", function () {
     textArea.innerText = "Started...";
@@ -17,6 +14,35 @@ newGameButton.addEventListener("click", function () {
     stayButton.style.display = "inline";
     hitButton.style.display = "inline";
 
+});
+
+let gameStarted = false,
+    gameOver = false,
+    playerWon = false,
+    dealerCards = [],
+    playerCards = [],
+    dealerScore = 0,
+    playerScore = 0,
+    deck = [];
+
+hitButton.style.display = "none";
+stayButton.style.display = "none";
+showStatus();
+
+newGameButton.addEventListener("click", function () {
+    gameStarted = true;
+    gameOver = false;
+    playerWon = false;
+
+    deck = createDeck();
+    shuffleDeck(deck);
+    dealerCards = [getNextCard(), getNextCard()];
+    playerCards = [getNextCard(), getNextCard()];
+
+    newGameButton.style.display = "none";
+    hitButton.style.display = "inline";
+    stayButton.style.display = "inline";
+    showStatus();
 });
 
 function createDeck() {
@@ -33,6 +59,18 @@ function createDeck() {
     return deck;
 }
 
+function showStatus() {
+    if (!gameStarted) {
+        textArea.innerText = "Welcome to Blackjack!";
+        return;
+    }
+
+    for (let i=0; i < deck.length; i++){
+        textArea.innerText += "\n" + getCardString(deck[i]);
+    }
+
+}
+
 function getCardString(card) {
     return card.value + " of " + card.suit;
 
@@ -42,6 +80,12 @@ function getNextCard() {
     return deck.shift();
 }
 
-let deck = createDeck();
-let playerCards = [getNextCard(), getNextCard()];
+function shuffleDeck() {
+    for (let i = 0; i < deck.length; i++) {
+        let swapIdx = Math.trunc(Math.random() * deck.length);
+        let tmp = deck[swapIdx];
+        deck[swapIdx] = deck[i];
+        deck[i] = tmp;
+    }
 
+}
